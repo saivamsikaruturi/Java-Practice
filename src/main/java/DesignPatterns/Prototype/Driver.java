a58fc8dee78206d763cb9e031fcded1c956b39c9
VAMSI KARUTURI <karuturi-sai-naga.vamsi-krishna@siemens.com>
2023-06-30 09:37:10 +0530

    builder factory dp

--- src/main/java/DesignPatterns/Prototype/Driver.java
+++ src/main/java/DesignPatterns/Prototype/Driver.java
@@ -2,7 +2,7 @@ package DesignPatterns.Prototype;
 
 public class Driver {
     public static void main(String[] args) {
-        Employee employee1 = new Employee(1,"Lava Kumar",24,10000000);
+        Employee employee1 = new Employee(1,"Vamsi Krishna",24,10000000);
 
         System.out.println(employee1);
         System.out.println(employee1.hashCode());
